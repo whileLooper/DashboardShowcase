7cc909b5c12f4f574abaf8f8820cfc638e1e41e4
Bo Chen <bochentheone@gmail.com>
2017-06-15 21:03:28 -0400

    real data added

--- src/app/mockup/mockup.component.ts
+++ src/app/mockup/mockup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MockupServices } from "./mockup.services";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'mockup-cmp',
@@ -10,39 +13,48 @@ export class MockupComponent {
   isLoading: boolean;
   overAllData: any;
   mainStatus: any;
+
+  totalInfo: any;
+
   constructor(private _service: MockupServices) {
     this.isLoading = true;
+    this.totalInfo = {};
   }
 
   ngOnInit(): void {
     this.getOverallInfo();
-    this.getRoomsDetail();
+    // this.getRoomsDetail();
   }
 
   public getOverallInfo() {
     // let result = this._service.getOverallInfo();
     // console.log(result);
-    this._service.getOverallInfo().subscribe(
+    let sub1 = this._service.getOverallInfo();
+    let sub2 = this._service.getRoomsDetail();
+
+    Observable.forkJoin(sub1, sub2).subscribe(
       (res: any) => {
-        this.overAllData = res.hasOwnProperty('Data') ? res.Data : undefined;
-        console.log(this.overAllData);
+        this.updateData(res);
       },
       (err: any) => {
-        console.error('房态图，访问链接信息报错');
+        console.error('访问链接信息报错');
       }
     );
   }
 
-  public getRoomsDetail() {
-    this._service.getRoomsDetail().subscribe(
-      (res: any) => {
-        this.mainStatus = res.hasOwnProperty('Data') ? res.Data : undefined;
-        console.log(this.mainStatus);
-      },
-      (err: any) => {
-        console.error('房间信息， 访问链接信息报错');
-      }
-    )
+  public updateData(res: any) {
+    this.overAllData = res[0].hasOwnProperty('Data') ? res[0].Data : undefined;
+    this.mainStatus = res[1].hasOwnProperty('Data') ? res[1].Data : undefined;
+
+    // set overall object value
+    this.totalInfo['Ad_DepFolio'] = this.overAllData['Ad_DepFolio'];  // 今日预离
+    this.totalInfo['Ad_FolioCount'] = this.overAllData['Ad_FolioCount'];  // 今日预抵
+    this.totalInfo['Ad_AllDebit'] = this.overAllData['Ad_AllDebit'];  // 预测营收
+    this.totalInfo['currOccupied'] = this.mainStatus.filter(e => e['CheckInState'] === 1).length;
+    this.totalInfo['currEmpty'] = this.mainStatus.filter(e => e['CheckInState'] === 2).length;
+    this.isLoading = false;
+    console.log(this.mainStatus);
   }
+
 }
 
